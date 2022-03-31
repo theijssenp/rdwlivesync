44530e29ab3d73cdcdc7967415e0eeca6557688d
Pieter Theijssen <p.theijssen@gmail.com>
2022-03-31 14:53:17 +0200

    Param datum changed

--- src/main/java/nl/hodc/rdwlivesync/rdw/RdwRequestData.java
+++ src/main/java/nl/hodc/rdwlivesync/rdw/RdwRequestData.java
@@ -11,18 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class RdwRequestData {
-    public static ResponseEntity<String> getData(String datumEersteAfgifte) {
+    public static ResponseEntity<String> getData(String zoekDatum) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
 
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         // request op basis van datum_eerste_afgifte_nederland bv 20201218
-        String resourceURL = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?$select=*&$limit=100000&$where=datum_tenaamstelling+=+'"+ datumEersteAfgifte + "'";
+        String resourceURL = "https://opendata.rdw.nl/resource/m9d7-ebf2.json?$select=*&$limit=100000&$where=datum_tenaamstelling+=+'"+ zoekDatum + "'";
 
         HttpEntity<String> entity = new HttpEntity<String>(headers);
         ResponseEntity<String> response = restTemplate.exchange(resourceURL, HttpMethod.GET, entity, String.class);
         if (response.getStatusCode() != HttpStatus.OK) {
-            System.out.println("Status request niet OK bij " + datumEersteAfgifte);
+            System.out.println("Status request niet OK bij " + zoekDatum);
         }
         return response;
     }
